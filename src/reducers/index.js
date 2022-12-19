a4631d136fd124dfe83147c6dede90cb32305a6a
Thomas Blumenfeld <thomas@roadmapper.fr>
2022-12-19 20:25:06 +0100

    display tweet ids

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+
+import users from './users'
+import tweets from './tweets'
+import authedUser from './authedUser'
+
+export default combineReducers({
+  authedUser,
+  users,
+  tweets
+})
